1eedf8ac8882bd8624bcbd58d953a0efdfbf98ef
kareemosman <kareemosman288@gmail.com>
2025-03-19 16:42:57 +0200

    Add files via upload

---
+++ asg_1.c
@@ -0,0 +1,20 @@
+//*********************p1**************************//
+#include<stdio.h>
+int main ()
+ {
+     int num;
+     printf("enter your number :"),
+     scanf("%d", &num);
+     if( num %2==0)
+     {
+         printf(" the number is even =1 \n");
+         printf("the number is odd = o \n");
+
+     }
+     else
+     {
+         printf(" the number is even =0 \n");
+         printf("the number is odd = 1 \n");
+
+     }
+ }
